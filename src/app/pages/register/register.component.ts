5762093f2e430511bebd2168fe26d453cf83ac28
unknown <boom.jaidee@hotmail.com>
2019-03-13 16:28:16 +0700

    pusH

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,7 +1,9 @@
+import { environment } from './../../../environments/environment.prod';
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "ng6-md-auth";
 import { NgxSpinnerService } from "ngx-spinner";
+import { TeameServiceService } from "src/app/services/teams-service/teame-service.service";
 
 @Component({
   selector: "app-register",
@@ -12,19 +14,23 @@ export class RegisterComponent implements OnInit {
   constructor(
     private userAuth: AuthService,
     private router: Router,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private teameService: TeameServiceService,
+
   ) {
-    this.userAuth.isLoggingIn.subscribe(()=>{
+    this.userAuth.isLoggingIn.subscribe(() => {
       this.spinner.show();
     });
-    this.userAuth.isLoggedIn.subscribe(value => {
+    this.userAuth.isLoggedIn.subscribe(async value => {
       this.spinner.hide();
-      if(this.userAuth.user){
+      let res: any = await this.teameService.me();
+      window.localStorage.setItem(environment.apiUrl + '@user', JSON.stringify(res));
+      if (this.userAuth.user) {
         this.router.navigate(["/home"]);
       }
     });
 
-    this.userAuth.isLoggedFail.subscribe(error=>{
+    this.userAuth.isLoggedFail.subscribe(error => {
       this.spinner.hide();
       console.log(error);
     });
