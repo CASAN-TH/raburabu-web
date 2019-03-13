e64fc061caa5cc0e6f3c78a5db4d5cf8217827d7
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-03-13 14:06:54 +0700

    ass

---
+++ src/app/services/order/order.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(public http: HttpClient,) { }
+
+  private authorizationHeader() {
+    const token = window.localStorage.getItem('token@raburabu-web-dev');
+    // console.log(token);
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    return headers;
+  }
+
+  orderList(){
+    return this.http.get('http://13.250.99.131:3002/api/products')
+  }
+}
