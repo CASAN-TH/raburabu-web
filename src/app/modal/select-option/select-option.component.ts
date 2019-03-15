c1e049a09e0692959f5b662cc9dbcce1c619db70
pure1234567a <pure1234567a@gmail.com>
2019-03-15 15:32:45 +0700

    select multi option

--- src/app/modal/select-option/select-option.component.ts
+++ src/app/modal/select-option/select-option.component.ts
@@ -20,6 +20,9 @@ export class SelectOptionComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  dataBinding: any;
+  qtyInput: any;
+  totalQty: any = 0;
   constructor(
     private _formBuilder: FormBuilder,
     public servicePorduct: ProductsService,
@@ -29,7 +32,7 @@ export class SelectOptionComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log(this.data);
+    this.dataBinding = this.data
     // this.getProduct();
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
@@ -41,17 +44,21 @@ export class SelectOptionComponent implements OnInit {
 
   selectProduct(i) {
     this.selectOption.push(i);
-    this.selectOption.forEach(e => {
-      this.checkIcon = e
-    })
-    console.log(this.selectOption);
   }
+
   next() {
     this.selectOption
   }
+
   done() {
+    this.totalQty = 0;
     this.data.option.forEach(element => {
       this.nameOption = element.name;
+    });
+    this.selectOption.forEach(res => {
+      // console.log(res);
+      this.totalQty = this.totalQty + parseInt(res.qty);
+      // console.log(this.totalQty);
     });
     let data: any = {
       name: this.data.name,
@@ -60,11 +67,40 @@ export class SelectOptionComponent implements OnInit {
         name: this.nameOption,
         value: this.selectOption
       }],
-      qty: this.qty,
-      amout: this.qty * this.data.price
+      amout: this.totalQty * this.data.price
     }
     console.log(data)
     this.sendData.emit(data);
     this.dialogRef.close('close');
   }
+
+  addQty(l, e) {
+    // console.log(e);
+    this.selectOption[l].qty = parseInt(e);
+  }
+
+  select(itm, i, k) {
+    if (!this.dataBinding.option[k].value[i].active) {
+      this.dataBinding.option[k].value[i].active = true
+      this.dataBinding.option[k].value[i].qty = 1
+      this.selectOption.push(itm);
+    } else {
+      this.dataBinding.option[k].value[i].active = false;
+      this.selectOption.forEach(option => {
+        if (itm.name === option.name) {
+          let j = this.selectOption.findIndex(function (data) { return data.name === itm.name })
+          console.log(j);
+          this.selectOption.splice(j, 1);
+        }
+      });
+    }
+    console.log(this.selectOption);
+  }
+
+
+
+
+
+
+
 }
