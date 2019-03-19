2bb39dc25dd1e63cf26a23169b022874c90c248b
unknown <boom.jaidee@hotmail.com>
2019-03-19 10:38:42 +0700

    pusH

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -5,6 +5,7 @@ import { MatDialog } from '@angular/material';
 import { SelectOptionComponent } from 'src/app/modal/select-option/select-option.component';
 import { OrderService } from 'src/app/services/order/order.service';
 import { environment } from 'src/environments/environment.prod';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-order',
@@ -35,7 +36,9 @@ export class OrderComponent implements OnInit {
     private prodService: ProductsService,
     public dialog: MatDialog,
     private orderService: OrderService,
-    public router: Router
+    public router: Router,
+    public ngxSpinner: NgxSpinnerService
+
   ) { }
 
   prodData: any;
@@ -54,10 +57,19 @@ export class OrderComponent implements OnInit {
   }
 
   async getProd() {
-    let res: any = await this.prodService.order();
-    // console.log(res);
-    this.prodData = res.data;
-    // console.log(this.prodData);
+    this.ngxSpinner.show();
+
+    try {
+      let res: any = await this.prodService.order();
+      // console.log(res);
+      this.prodData = res.data;
+      this.ngxSpinner.hide();
+      // console.log(this.prodData);
+    } catch (error) {
+      this.ngxSpinner.hide();
+
+    }
+
   }
 
   openmodal(i) {
@@ -81,36 +93,44 @@ export class OrderComponent implements OnInit {
   }
 
   async onSave() {
-    let user: any = JSON.parse(window.localStorage.getItem(environment.apiUrl + '@user'));
-    let body = {
-      customer: {
-        firstname: this.address.firstname,
-        lasname: this.address.lastname,
-        tel: this.address.tel,
-        address: [
-          {
-            houseno: this.address.address.houseno,
-            village: this.address.address.village,
-            street: this.address.address.street,
-            subdistrict: this.address.address.subdistrict,
-            district: this.address.address.district,
-            province: this.address.address.province,
-            zipcode: this.address.address.zipcode
-          }
-        ]
-      },
-      items: this.data.items,
-      paymenttype: {
-        name: this.namePayment
-      },
-      totalamount: this.data.totalamount,
-      user_id: user.data._id
+    this.ngxSpinner.show();
+    try {
+      let user: any = JSON.parse(window.localStorage.getItem(environment.apiUrl + '@user'));
+      let body = {
+        customer: {
+          firstname: this.address.firstname,
+          lasname: this.address.lastname,
+          tel: this.address.tel,
+          address: [
+            {
+              houseno: this.address.address.houseno,
+              village: this.address.address.village,
+              street: this.address.address.street,
+              subdistrict: this.address.address.subdistrict,
+              district: this.address.address.district,
+              province: this.address.address.province,
+              zipcode: this.address.address.zipcode
+            }
+          ]
+        },
+        items: this.data.items,
+        paymenttype: {
+          name: this.namePayment
+        },
+        totalamount: this.data.totalamount,
+        user_id: user.data._id
+      }
+      console.log(body);
+      console.log(this.data.items);
+      let res: any = await this.orderService.saveOrder(body);
+      console.log(res);
+      this.ngxSpinner.hide();
+      this.router.navigate(['/order-list']);
+    } catch (error) {
+      this.ngxSpinner.hide();
+
     }
-    console.log(body);
-    console.log(this.data.items);
-    let res: any = await this.orderService.saveOrder(body);
-    console.log(res);
-    // this.router.navigate(['/order-list']);
+
   }
 
   selectPaymentType(item) {
