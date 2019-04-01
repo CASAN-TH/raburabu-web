6b4f3251ee61af29ded50df284ff6cdc3264c131
Natsulelou <nutshapon_lelou@hotmail.com>
2019-04-01 10:20:08 +0700

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,5 @@ export const environment = {
   production: false,
   environment: "prod",
   appName: "raburabu-web",
-<<<<<<< HEAD
   apiUrl: "http://13.229.80.32"
-=======
-  apiUrl: "http://3.1.213.121"
-
->>>>>>> 7014ce6f921bfd6d823b7cdd826b7c49dbb0165a
 };
