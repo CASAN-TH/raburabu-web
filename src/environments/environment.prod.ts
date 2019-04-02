942547d56dee0a0d443399590fadf0008d08e879
Natsulelou <nutshapon_lelou@hotmail.com>
2019-04-02 17:21:27 +0700

    chg apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   environment: "prod",
   appName: "raburabu-web",
-  apiUrl: "http://13.229.80.32"
+  // apiUrl: "http://13.229.80.32"
+  apiUrl: "http://54.251.134.2"
 };
