15d7ede4c7d8df3497da2c68e7bf7761ae00f98e
pure1234567a <pure1234567a@gmail.com>
2019-04-05 11:45:10 +0700

    default page

---
+++ src/app/pages/blank/blank.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-blank',
+  templateUrl: './blank.component.html',
+  styleUrls: ['./blank.component.scss']
+})
+export class BlankComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private spinner: NgxSpinnerService,
+  ) { }
+
+  ngOnInit() {
+    let res: any = JSON.parse(window.localStorage.getItem(environment.apiUrl + '@user'));
+    // console.log(res);
+    if (res && res.data) {
+      if (res.data.roles[0] === 'user') {
+        if (res.data.ref1) {
+          if (res.data.ref1 === '') {
+            this.router.navigate(["/home"]);
+          } else {
+            this.router.navigate(["/manage-member"]);
+          }
+        } else {
+          this.router.navigate(["/home"]);
+        }
+      }
+      if (res.data.roles[0] === 'stockstaff') {
+        this.router.navigate(["/monitor"]);
+      }
+      if (res.data.roles[0] === 'packstaff') {
+        this.router.navigate(["/monitor"]);
+      }
+      if (res.data.roles[0] === 'admin') {
+        this.router.navigate(["/admin-manage-team"]);
+      }
+      if (res.data.roles[0] === 'owner') {
+        this.router.navigate(["/manage-member"]);
+      }
+      if (res.data.roles[0] === 'staff') {
+        this.router.navigate(["/manage-member"]);
+      }
+    } else {
+      this.router.navigate(["/login"]);
+    }
+  }
+
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -39,6 +39,7 @@ export class LoginComponent implements OnInit {
     private snackBar: MatSnackBar,
     private teameService: TeameServiceService,
   ) {
+    // this.userAuth.isLoggingIn.observers=[]
     this.userAuth.isLoggingIn.subscribe(() => {
       this.spinner.show();
     });
@@ -57,10 +58,14 @@ export class LoginComponent implements OnInit {
         this.router.navigate(["/admin-manage-team"]);
       }
       if (res.data.roles[0] === 'user') {
-        if (res.data.ref1 === '') {
+        if (res.data.ref1) {
+          if (res.data.ref1 === '') {
+            this.router.navigate(["/home"]);
+          } else {
+            this.router.navigate(["/manage-member"]);
+          }
+        } else {
           this.router.navigate(["/home"]);
-        } else if (res.data.ref1 !== '') {
-          this.router.navigate(["/manage-member"]);
         }
       }
       if (res.data.roles[0] === 'owner') {
@@ -86,34 +91,6 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-    // let res: any = JSON.parse(window.localStorage.getItem(environment.apiUrl + '@user'));
-    // // console.log(res);
-
-    // if (res && res.data) {
-    //   if (res.data.roles[0] === 'user') {
-    //     if (res.data.ref1 === '') {
-    //       this.router.navigate(["/home"]);
-    //     } else if (res.data.ref1 !== '') {
-    //       this.router.navigate(["/manage-member"]);
-    //     }
-    //   }
-    //   if (res.data.roles[0] === 'stockstaff') {
-    //     this.router.navigate(["/monitor"]);
-    //   }
-    //   if (res.data.roles[0] === 'packstaff') {
-    //     this.router.navigate(["/monitor"]);
-    //   }
-    //   if (res.data.roles[0] === 'admin') {
-    //     this.router.navigate(["/admin-manage-team"]);
-    //   }
-    //   if (res.data.roles[0] === 'owner') {
-    //     this.router.navigate(["/manage-member"]);
-    //   }
-    //   if (res.data.roles[0] === 'staff') {
-    //     this.router.navigate(["/manage-member"]);
-    //   }
-    // }
-  }
+  ngOnInit() {}
 
 }
