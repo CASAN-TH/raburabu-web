613057e9939f76f18b7be5d1fe3d78afdbc0d437
Theerasak Tubrit <jigkoh3@Theerasaks-MacBook-Pro.local>
2019-04-06 07:47:52 +0700

    123

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   environment: "prod",
   appName: "raburabu-web",
   // apiUrl: "http://13.229.80.32"
-  apiUrl: "http://52.77.235.7"
+  apiUrl: "https://raburabu-api.lamunphan.com"
   // apiUrl: "http://18.136.120.17"
 };
