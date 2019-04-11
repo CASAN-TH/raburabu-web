dbc05c79477ec66cef66d7f9468a2a1448c3f887
pure1234567a <pure1234567a@gmail.com>
2019-04-11 14:21:36 +0700

    v spinner

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -49,42 +49,12 @@ export class LoginComponent implements OnInit {
     this.userAuth.isLoggedIn.subscribe(async value => {
       // this.spinner.hide();
       const token = window.localStorage.getItem(`token@${environment.appName}-${environment.environment}`);
-      // console.log(token);
       if (token && this.user === null) {
-        // console.log('1');
         this.user = this.userAuth.user;
         let res: any = await this.teameService.me();
         window.localStorage.setItem(environment.apiUrl + '@user', JSON.stringify(res));
-        // console.log(res);
         this.router.navigate(["/blank"]);
-        //   if (res.data.roles[0] === 'stockstaff') {
-        //     this.router.navigate(["/monitor"]);
-        //   }
-        //   if (res.data.roles[0] === 'packstaff') {
-        //     this.router.navigate(["/monitor"]);
-        //   }
-        //   if (res.data.roles[0] === 'admin') {
-        //     this.router.navigate(["/admin-manage-team"]);
-        //   }
-        //   if (res.data.roles[0] === 'user') {
-        //     if (res.data.ref1) {
-        //       if (res.data.ref1 === '') {
-        //         this.router.navigate(["/home"]);
-        //       } else {
-        //         this.router.navigate(["/manage-member"]);
-        //       }
-        //     } else {
-        //       this.router.navigate(["/home"]);
-        //     }
-        //   }
-        //   if (res.data.roles[0] === 'owner') {
-        //     this.router.navigate(["/manage-member"]);
-        //   }
-        //   if (res.data.roles[0] === 'staff') {
-        //     this.router.navigate(["/manage-member"]);
-        //   }
       }
-
     });
     this.userAuth.isLoggedFail.observers = []
     this.userAuth.isLoggedFail.subscribe(error => {
@@ -105,6 +75,6 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.spinner.hide();
-   }
+  }
 
 }
