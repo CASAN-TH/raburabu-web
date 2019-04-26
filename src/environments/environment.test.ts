afcda09864af50e44444656cd60958ff7db8ebeb
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-04-26 19:59:23 +0700

    ass

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,7 +3,7 @@ export const environment = {
     environment: "test",
     appName: "raburabu-web",
     // apiUrl: "http://18.136.120.17"
-    apiUrl: "http://13.229.80.32"
-    
+    // apiUrl: "http://13.229.80.32"
+    apiUrl: "http://52.77.235.7"
     // apiUrl: "http://54.251.134.2"
 };
