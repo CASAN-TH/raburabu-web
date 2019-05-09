e3a1a195b7bb182475f6a078c2b8e1488eee79e7
pure1234567a <pure1234567a@gmail.com>
2019-05-09 13:57:07 +0700

    check activate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     redirectTo: "blank",
     pathMatch: "full"
   },
-  { path: "blank", component: BlankComponent },
+  { path: "blank", component: BlankComponent, canActivate: [AuthGuardService]  },
   { path: "home", component: HomeComponent, canActivate: [AuthGuardService] },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
