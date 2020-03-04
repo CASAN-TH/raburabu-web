24685d3b69e39d77031e7bdf8f55951b1b675653
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-03-04 20:06:06 +0700

    update v2

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -4,6 +4,6 @@ export const environment = {
     appName: "raburabu-web",
     // apiUrl: "http://18.136.120.17"
     // apiUrl: "http://13.229.80.32"
-    apiUrl: "http://52.77.235.7"
+    apiUrl: "https://raburabu.cyberadvancesystem.com"
     // apiUrl: "http://54.251.134.2"
 };
